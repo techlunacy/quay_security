38dfab3f3db5375a8a6bb7f7837e155371defa9c
Graeme <graeme@techlunacy.com>
2019-06-30 14:03:25 +0800

    handle the stin options with some refactoring

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
-# Minimum Requirements:
+# Quay security scan
+Recieve a list of images in internal format and return a list of CVEs and of the metadata of the images.
+
+## Minimum Requirements: 
 1. Python 2.7
 2. pip
 3. Linux/MacOS
-# Installation
-1. pip install -r requirements.txt
 
-# Run
-```
-./quay.py test.json
-```
-```
-./quay.py
+## Installation 
+1. pip install -r requirements.txt 
+
+## Run 
+As a parameter:  
+```  
+./quay.py test.json  
+```  
+Reading from stdin:
+```  
+cat test.json | ./quay.py 
 ```
--- quay.py
+++ quay.py
@@ -3,18 +3,32 @@ import json
 import pprint
 import sys
 import requests
-filepath = sys.argv[1]
+
+
+def get_security_results(images):
+    for image in images:
+        repo = "{}/{}".format(image["Organisation"], image["Repository"])
+        tag = image['Tag']
+        repository_response = requests.get(
+            'https://quay.io/api/v1/repository/{}?includeTags=true'.format(repo))
+        repository_response.raise_for_status()
+        repository = repository_response.json()
+        manifest = repository['tags'][tag]['manifest_digest']
+        security_results_response = requests.get(
+            'https://quay.io/api/v1/repository/{repository}/manifest/{manifest}/security?vulnerabilities=true'.format(repository=repo, manifest=manifest))
+        security_results_response.raise_for_status()
+        security_results = security_results_response.json()
+        image.update(security_results)
+        results.append(image)
+        return results
 pp = pprint.PrettyPrinter(indent=4)
 results = []
-with open(filepath, 'r') as outfile:  
-    images = json.load(outfile)
-    for image in images:
-      repo = "{}/{}".format(image["Organisation"],image["Repository"])
-      tag = image['Tag']
-      repository = requests.get('https://quay.io/api/v1/repository/{}?includeTags=true'.format(repo)).json()
-      manifest = repository['tags'][tag]['manifest_digest']
-      # pp.pprint(repository)
-      security_results = requests.get('https://quay.io/api/v1/repository/{repository}/manifest/{manifest}/security?vulnerabilities=true'.format(repository=repo,manifest=manifest)).json()
-      image.update(security_results)
-      results.append(image)
+images = []
+if(len(sys.argv) == 2):
+    filepath = sys.argv[1]
+    with open(filepath, 'r') as outfile:
+        images = json.load(outfile)
+else:
+    images = json.load(sys.stdin)
+results = get_security_results(images)
 print(json.dumps(results, indent=4, sort_keys=True))
